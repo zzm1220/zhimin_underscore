297dfcb921e836fd589e77d308d1ad8f169dce29
zhaozhimin <zhaozhimin@chinaiptv.cn>
2022-01-17 17:49:52 +0800

    init project

---
+++ underscore.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: zhimin
+ * @Date: 2022-01-17 17:44:12
+ * @LastEditors: zhimin
+ * @LastEditTime: 2022-01-17 17:49:12
+ * @FilePath: \zhimin_underscore\underscore.js
+ */
+(function () {
+  var root = (typeof self == 'object' && self.self == self && self) ||
+    (typeof global == 'global' && global.global == global && global)
+})();
